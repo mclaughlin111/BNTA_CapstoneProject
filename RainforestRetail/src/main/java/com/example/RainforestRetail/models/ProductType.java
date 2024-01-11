cf4d055e032a0bd87ea7a5bc59671adeeada6192
Joseph Vincenzo Carpentieri <joecarpentieri1@gmail.com>
2024-01-11 11:06:48 +0000

    added ProductType class

---
+++ RainforestRetail/src/main/java/com/example/RainforestRetail/models/ProductType.java
@@ -0,0 +1,19 @@
+package com.example.RainforestRetail.models;
+
+public enum ProductType {
+
+    HOMEGOODS,
+    ELECTRONICS,
+    ENTERTAINMENT,
+    BEAUTY,
+    HOBBIES,
+    SUSTAINABILITY;
+
+    
+
+}
+
+//enum Homegoods {
+//    MUGS,
+//
+//}
