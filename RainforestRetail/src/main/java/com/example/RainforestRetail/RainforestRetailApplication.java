3cd731852830400adbea4038d9b127794c38e3d4
Rashad Ramali <rashad.ramali@hotmail.co.uk>
2024-01-11 09:37:09 +0000

    created initial Spring server

---
+++ RainforestRetail/src/main/java/com/example/RainforestRetail/RainforestRetailApplication.java
@@ -0,0 +1,13 @@
+package com.example.RainforestRetail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainforestRetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainforestRetailApplication.class, args);
+	}
+
+}
