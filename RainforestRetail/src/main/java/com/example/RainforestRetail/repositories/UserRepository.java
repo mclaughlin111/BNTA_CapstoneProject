e347c76c6f79e08885805b45b3041d94947190a4
Joseph Vincenzo Carpentieri <joecarpentieri1@gmail.com>
2024-01-15 12:36:19 +0000

    creating a method to find a user by email

--- RainforestRetail/src/main/java/com/example/RainforestRetail/repositories/UserRepository.java
+++ RainforestRetail/src/main/java/com/example/RainforestRetail/repositories/UserRepository.java
@@ -1,1 +1,1 @@
-package com.example.RainforestRetail.repositories;import com.example.RainforestRetail.models.User;import org.springframework.data.jpa.repository.JpaRepository;public interface UserRepository extends JpaRepository <User, Long> {}
+package com.example.RainforestRetail.repositories;import com.example.RainforestRetail.models.User;import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.data.jpa.repository.Query;public interface UserRepository extends JpaRepository <User, Long> {    @Query    User findByEmail(String email);}
--- rainforestretailclient/src/Components/CheckoutForm.js
+++ rainforestretailclient/src/Components/CheckoutForm.js
@@ -8,13 +8,13 @@ const CheckoutForm = () => {
     <>
       <h1>Checkout</h1>
       <form id="checkoutForm">
-        <label htmlFor="customer-name">Customer Name:</label>
+        {/* <label htmlFor="customer-name">Customer Name:</label>
         <input
           id="customer-name"
           name="Name"
           type="text"
           placeholder="enter name"
-        />
+        /> */}
 
         <label id>Customer Email Address:</label>
         <input
@@ -24,13 +24,13 @@ const CheckoutForm = () => {
           placeholder="enter email"
         />
 
-        <label id>Customer Home Address:</label>
+        {/* <label id>Customer Home Address:</label>
         <input
           id="customer-home-address"
           name="Home Address"
           type="text"
           placeholder="enter home address"
-        />
+        /> */}
         <input type="submit" value="Buy Now"/>   
       </form>
     </>
