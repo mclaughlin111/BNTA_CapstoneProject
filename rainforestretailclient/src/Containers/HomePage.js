0a1b8318e7d8621b8f9a6750ed1ba0ce4bcaefc1
Thomas McLaughlin <tmclaughlin5.1111@gmail.com>
2024-01-18 12:31:54 +0000

    styled nav bar with change on scroll

--- rainforestretailclient/src/Containers/HomePage.js
+++ rainforestretailclient/src/Containers/HomePage.js
@@ -2,7 +2,7 @@ import Basket from "../Components/Basket";
 import CheckoutForm from "../Components/CheckoutForm";
 import ProductList from "../Components/ProductList";
 import { useState, useEffect } from "react";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "../Components/Home";
 import CompletedOrder from "../Components/CompletedOrder";
 
@@ -13,8 +13,7 @@ const HomePage = () => {
   const [loading, setLoading] = useState(true); // state for loading function
   const [user, setUser] = useState({});
   const [order, setOrder] = useState({});
-  const [filteredProducts, setFilteredProducts]= useState([]);
-  
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
   //FETCH Products
   const fetchProducts = async () => {
@@ -24,29 +23,33 @@ const HomePage = () => {
   };
 
   const fetchEmail = async (enteredEmail) => {
-    
-    const response = await fetch(`http://localhost:8080/users?email=${enteredEmail}`)
+    const response = await fetch(
+      `http://localhost:8080/users?email=${enteredEmail}`
+    );
     const data = await response.json();
     setUser(data);
-  }
+  };
 
   const createNewOrder = async () => {
-    const response = await fetch(`http://localhost:8080/orders?userId=${user.id}`,{
-      method: "POST",
-      headers: {"Content-Type":"application/json"},
-      body: JSON.stringify(basket)
-    })
+    const response = await fetch(
+      `http://localhost:8080/orders?userId=${user.id}`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(basket),
+      }
+    );
     const order = await response.json();
     setOrder(order);
-    
-  }
+  };
 
-  const fetchFilteredProducts = async(productType)=>{
-    const response = await fetch(`http://localhost:8080/products/filtered?productType=${productType}`)
+  const fetchFilteredProducts = async (productType) => {
+    const response = await fetch(
+      `http://localhost:8080/products/filtered?productType=${productType}`
+    );
     const data = await response.json();
-    setProducts(data)
-  }
-  
+    setProducts(data);
+  };
 
   //Function Adds User selected products to basket
   const handleClickToBasket = (product) => {
@@ -57,16 +60,12 @@ const HomePage = () => {
 
     // set basket for qauntity
     setBasket((prevBasket) => {
-        const newBasket = { ...prevBasket };
-        newBasket[product] = (newBasket[product] || 0) + 1;
-       // Updated basket with product quantity
-        return newBasket;
-      });
-
+      const newBasket = { ...prevBasket };
+      newBasket[product] = (newBasket[product] || 0) + 1;
+      // Updated basket with product quantity
+      return newBasket;
+    });
   };
-
- 
-  
 
   useEffect(() => {
     fetchProducts().finally(() => setLoading(false)); // load function on fetchProducts
@@ -74,55 +73,53 @@ const HomePage = () => {
 
   const rainforestRoutes = createBrowserRouter([
     {
-        path: "/",
-        element: <Home 
-        />,
-        children: [
-            {
-                path: "/basket",
-                element:  <Basket 
-                products={products} 
-                basketItems={basketItems} 
-                basket={basket}/>
-               
-      
-            },
-            {
-                path: "/checkout",
-                element: <CheckoutForm
-                fetchEmail={fetchEmail}
-                createNewOrder={createNewOrder}
-                fetchProducts={fetchProducts}
-                basketItems={basketItems}
-                basket={basket}
-                />
-            },
-            {
-              path:"/",
-              element: <ProductList
+      path: "/",
+      element: <Home fetchFilteredProducts={fetchFilteredProducts} />,
+      children: [
+        {
+          path: "/basket",
+          element: (
+            <Basket
+              products={products}
+              basketItems={basketItems}
+              basket={basket}
+            />
+          ),
+        },
+        {
+          path: "/checkout",
+          element: (
+            <CheckoutForm
+              fetchEmail={fetchEmail}
+              createNewOrder={createNewOrder}
+              fetchProducts={fetchProducts}
+              basketItems={basketItems}
+              basket={basket}
+            />
+          ),
+        },
+        {
+          path: "/",
+          element: (
+            <ProductList
               products={products}
               handleClickToBasket={handleClickToBasket}
               basket={basket}
               filteredProducts={filteredProducts}
-              fetchFilteredProducts={fetchFilteredProducts}
-              />  
-            },
-            {
-              path:"/completed-order",
-              element: <CompletedOrder
-              order={order}
-              />
-              
-            }
-            
-        ]
-    }
-]);
+            />
+          ),
+        },
+        {
+          path: "/completed-order",
+          element: <CompletedOrder order={order} />,
+        },
+      ],
+    },
+  ]);
 
   return (
     <>
-      <RouterProvider router = {rainforestRoutes} />
-
+      <RouterProvider router={rainforestRoutes} />
     </>
   );
 };
