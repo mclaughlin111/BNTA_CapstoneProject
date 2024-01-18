d42f7f243cdbde3697612e01016e611be692dd0a
Thomas McLaughlin <tmclaughlin5.1111@gmail.com>
2024-01-18 10:34:42 +0000

    styling in progress

--- rainforestretailclient/src/Components/Basket.js
+++ rainforestretailclient/src/Components/Basket.js
@@ -1,19 +1,31 @@
 import { Navigate, useNavigate } from "react-router-dom";
+import '../Basket.css'
 
 const Basket = ({ basketItems, products, basket}) => {
 
   const productsWithQuantity = (basket) => {
     return (  
         
-    <ul>
-        
-        {Object.entries(basket).map(([idItem, quant], id) => (
-          <li key={id}>
-            {products[idItem-1].name} : {quant}
-          </li>
-        ))}
-      </ul>
-      )
+      <div>
+          {Object.entries(basket).map(([idItem, quant], id) => (
+            <div className="eachItem" key={id}>
+              <img className="basketImage" alt="product image" src={products[idItem-1].productImg}></img>
+              <span>{products[idItem-1].name}</span> 
+              <span>{quant}</span> 
+            </div>
+          ))}
+      </div>
+
+  /* <ul>
+  {Object.entries(basket).map(([idItem, quant], id) => (
+    <li className="eachItem" key={id}>
+      <img className="basketImage" alt="product image" src={products[idItem-1].productImg}></img> : {products[idItem-1].name} : {quant} 
+    </li>
+  ))}
+  </ul> */
+
+      
+    )
   
   };
 
@@ -24,14 +36,20 @@ const Basket = ({ basketItems, products, basket}) => {
   }
  
   return (
-    <>
-     
+    
+    <div className="basketContainer">
       <h2>
-        Basket<span className="basketTotal">{basketItems.length}</span>
+        Basket
+        <span className="basketTotal">{basketItems.length}</span>
       </h2>
-      {productsWithQuantity(basket)}
-      <button onClick={handleClick}>Go To Checkout</button>
-    </>
+      <div className="basketItems">
+        {productsWithQuantity(basket)}
+      </div>  
+      <button className="checkoutButton" onClick={handleClick}>
+        Go To Checkout
+      </button>
+    </div>  
+    
   );
 };
 
--- rainforestretailclient/src/Components/Home.js
+++ rainforestretailclient/src/Components/Home.js
@@ -1,25 +1,28 @@
-import {Link, Outlet} from "react-router-dom";
-
+import { Link, Outlet } from "react-router-dom";
+// import { FaCartShopping } from "react-icons/fa6";
+import "../Home.css";
 
 const Home = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <ul className="navbarList">
+          <li>
+            <h1>
+              <Link to="/">Home</Link>
+            </h1>
+          </li>
 
-    return ( 
-        <>
-            <nav>
-                <ul>
-                    <li><Link to= "/basket">Basket</Link> </li>
-                    <li><Link to= "/checkout">Checkout</Link></li>
-                    <li><Link to="/">Home</Link></li>
-                    
-                </ul>
-            
-                <Outlet/>
+          <li>
+          {/* <FaCartShopping /> */}
+            <h2><Link to="/basket">Basket<span></span></Link>
+            <Link to="/checkout">Checkout</Link></h2>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
 
-                  
-            </nav>
-        </>
-
-     );
-}
- 
 export default Home;
